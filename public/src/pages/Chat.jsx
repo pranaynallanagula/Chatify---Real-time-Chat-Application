6e4e95385ed70c0583872d20e666ed410ec8730f
Pranay Raj Nallanagula <89977392+pranaynallanagula@users.noreply.github.com>
2023-06-12 03:38:04 +0200

    Added the code from local storage

---
+++ public/src/pages/Chat.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect, useRef } from 'react';
+import styled from "styled-components";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute, host } from "../utils/APIRoutes";
+import Contacts from "../components/Contacts";
+import Welcome from "../components/Welcome";
+import ChatContainer from "../components/ChatContainer";
+import { io } from "socket.io-client"
+
+function Chat() {
+  const socket = useRef();
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    const checkUser = async () => {
+      if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+        navigate("/login");
+      } else {
+        setCurrentUser(
+          JSON.parse(
+            localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+          )
+        );
+        setIsLoaded(true);
+      }
+    };
+
+    checkUser();
+  }, []);
+
+  useEffect(() => {
+    if (currentUser) {
+      socket.current = io(host);
+      socket.current.emit("add-user", currentUser._id);
+    }
+  }, [currentUser]);
+
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    };
+
+    fetchContacts();
+  }, [currentUser]);
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  }
+
+  return (
+    <>
+      <Container>
+        <div className="container">
+          <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
+          {
+            isLoaded && currentChat == undefined ? (<Welcome currentUser={currentUser} />) : (<ChatContainer currentChat={currentChat} currentUser={currentUser} socket={socket} />)
+          }
+
+        </div>
+      </Container>
+
+
+    </>
+
+  )
+}
+
+export default Chat
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
